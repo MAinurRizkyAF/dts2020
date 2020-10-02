0b7885059e096d8ab71dcfea9355ef228a9502a7
ainur rizky <ainur.rizqi.ae@gmail.com>
2020-10-02 20:13:14 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/rizkysalossa/dts/MainUtama.java
@@ -0,0 +1,13 @@
+package com.example.rizkysalossa.dts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_utama);
+    }
+}
